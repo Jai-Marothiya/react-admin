fb368c3d9c51eba7b36553639c4bba7495f3d1fe
Jai Marothiya <jaimarothiya48@gmail.com>
2023-07-30 18:37:21 +0530

    Change Admin name and img

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -108,7 +108,7 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Jai Marothiya
+                  Ratan Tata
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
                   VP Fancy Admin
